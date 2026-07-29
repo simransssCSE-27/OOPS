e098cb1671234f364f6d066bb9f2bc90210cb78f
simransssCsE-27 <simranyadav20180093@gmail.com>
2026-07-29 14:38:28 +0530

    oops

---
+++ oops/oops.cpp
@@ -0,0 +1,36 @@
+//Simran yadav
+// 2503201001161
+
+#include <iostream>
+using namespace std;
+class students{
+    private:
+    // data member (attributes)
+    int rollno;
+    string name;
+    float marks;
+
+    public:
+    void input(){
+        cout<<"enter roll no";
+
+        cin>> roll no;
+        cin.ignore();// ignore new line character
+
+        cout<<"enter your name";
+         getline(cin,name); 
+
+         
+
+    void display(){
+    student s1;
+    s1.input;
+    s1.display();
+
+    }
+        
+    }
+        
+    
+    }
+}
